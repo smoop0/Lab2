f05044026ab48cb6db95c33ac8a4e5ee8bfdf529
JoshuaJed <joshuajedabiog@gmail.com>
2024-10-30 11:44:37 +0800

    added code for calculating median function

--- Lab2.py
+++ Lab2.py
@@ -5,6 +5,7 @@ def main():
     floatlist = get_user_input()
     calc_average(floatlist)
     find_min_max(floatlist)
+    calc_median_temperature(floatlist)
 
 
 def display_main_menu():
@@ -14,8 +15,9 @@ def display_main_menu():
 def calc_average(inputList):
     print("calc_average")
     total = sum(inputList)
-    calcAvg = sum / len(inputList)
-    print("Average =" + calcAvg)
+    length = len(inputList)
+    calcAvg = total / length
+    print("Average =", calcAvg)
     return calcAvg
 
 def get_user_input():
@@ -34,16 +36,27 @@ def find_min_max(inputList):
     inputList.sort() #affects original list
     resultList = [inputList[0], inputList[-1]] 
     # Use "-1" to select the last element in a list
-    print("Min & Max list = " + resultList)
+    print("Min & Max list = ", resultList)
     return resultList
 
 def sort_temperature():
     print("sort_temperature")
 
-def calc_median_temperature():
+def calc_median_temperature(inputList):
     print("calc_median_temperature")
 
+    midItem = len(inputList)
 
+    if midItem % 2 is 1:
+        median = inputList[int((midItem-1)/2)]
+    else:
+        upperMed = midItem/2
+        lowerMed = upperMed-1
+        sum = upperMed+lowerMed
+        median = sum/2
+    
+    print("Median = ", median)
+    return median
 
 
 
